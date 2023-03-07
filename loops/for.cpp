2a03dcfbb79d3878e920e80e462f084de8ae9941
ferreirazdev <business.flavioferreira@gmail.com>
2023-03-07 06:53:27 -0300

    feat: break & continue

--- loops/for.cpp
+++ loops/for.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 int main() {
-    for (int i = 0; i <= 15; i+=3) {
-        std::cout << i << " SALVE QUEBRADA\n";
+    for (int i = 1; i <= 25; i++) {
+        if (i == 13) {
+            continue;
+        }
+
+        if (i == 18) {
+            break;
+        }
+
+        std::cout << i << "\n";
     }
 
     return 0;
